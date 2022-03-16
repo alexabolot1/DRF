497e5cb23b7e84402e2d14cd738d69fa22c011c1
alex <alexabolot@gmail.com>
2022-03-16 22:24:02 +0500

    Корректировка д/з к третьему уроку

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer, StringRelatedField
 from todo.models import Project, Notes
+from users.serializers import UserModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
-
-    users = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Project
@@ -13,9 +12,6 @@ class ProjectModelSerializer(ModelSerializer):
 
 
 class NotesModelSerializer(ModelSerializer):
-
-    author_note = serializers.StringRelatedField()
-    project_name = serializers.StringRelatedField()
 
     class Meta:
         model = Notes
