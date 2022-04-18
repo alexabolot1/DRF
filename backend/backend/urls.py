6e28a714642b79ababb4fbf097d9479099a90f58
alex <alexabolot@gmail.com>
2022-04-18 21:01:29 +0500

    Корректировка домашнего задания к 9-ому уроку

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from todo.views import ProjectCustomViewSet, NotesCustomViewSet
@@ -8,7 +8,6 @@ from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 router = DefaultRouter()
 router.register('users', UserCustomViewSet)
 router.register('projects', ProjectCustomViewSet)
@@ -16,20 +15,22 @@ router.register('notes', NotesCustomViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
-        title='Library',
+        title='Backend',
         default_version='1.0',
         description='description',
-        contact=openapi.Contact(email='test@mail.com'),
+        contact=openapi.Contact(email='djagno@mail.com'),
         license=openapi.License(name='MIT')
     ),
     public=True,
-    permission_classes=(AllowAny, )
+    permission_classes=(AllowAny,)
 )
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('api-auth/', include('rest_framework.urls')),
-   path('api/', include(router.urls)),
-   path('api-token-auth/', views.obtain_auth_token),
-   path('swagger/', schema_view.with_ui()),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)', schema_view.without_ui()),
 ]
